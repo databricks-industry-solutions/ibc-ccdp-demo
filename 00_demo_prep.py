492c8a00c400541e39e5cabee678120a82277d95
danmorris-427 <danmorris-427>
2023-09-11 15:27:14 +0000

    added demo prep

--- 00_demo_prep.py
+++ 00_demo_prep.py
@@ -1,2 +1,15 @@
 # Databricks notebook source
+subscriber_data = "https://raw.githubusercontent.com/databricks-industry-solutions/ibc-ccdp-demo/main/data/subscribers.csv"
+svod_data = "https://raw.githubusercontent.com/databricks-industry-solutions/ibc-ccdp-demo/main/data/svod.csv"
+
+# COMMAND ----------
+
+_ = spark.sql("CREATE CATALOG IF NOT EXISTS ibc")
+
+# COMMAND ----------
+
+_ = spark.sql("CREATE DATABASE IF NOT EXISTS ibc.ccdp_demo")
+
+# COMMAND ----------
+
 
